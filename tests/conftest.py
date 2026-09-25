937e3ec4ca4cba18e79027439c9cb023f57b47a4
davidprender1 <150245329+davidprender1@users.noreply.github.com>
2026-09-25 15:23:36 +0000

    Added test for health

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,12 @@ from fastapi.testclient import TestClient
 
 from app.main import app, users
 
+
 @pytest.fixture(autouse=True)
 def clear_users():
     users.clear()
 
+
+@pytest.fixture
 def client():
     return TestClient(app)
